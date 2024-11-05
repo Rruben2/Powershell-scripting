18d939333ccbb57f90bcd928f05a41b8270dad9d
Rruben2 <rramosmombiela01.cf@iesesteveterradas.cat>
2024-11-05 16:21:20 +0100

    Add files via upload

---
+++ act6ej1.ps1
@@ -0,0 +1,11 @@
+﻿$ruta = read-host "Introduca la ruta C:\"
+$archivo = read-host "Introduzca el nombre del archivo o carpeta"
+
+$localizacion= Get-ChildItem -Path $ruta -Filter $archivo #Creo localizacion para encontrar el archivo en la ruta deseada
+
+If ($localizacion) {
+write-host "Archivo o directorio encontrado en"$localizacion.FullName #Si el archivo esta en la ruta escribo la ruta del archivo
+}
+else{
+write-Host "No se encontro el" $archivo "en" $ruta
+}
---
+++ act6ej2.ps1
@@ -0,0 +1,12 @@
+﻿$origen = read-host "Escribe la ruta del directorio a copiar"
+$destino = read-host "Escribe la ruta de destino"
+
+If (Test-Path -Path $origen )#Confirmo si la carpeta existe
+{
+    Copy-Item -Path $origen\* -Destination $destino -Recurse #Copio todo el contenido de la carpeta
+    Write-Host "Copia de seguridad completada con exito"
+}
+else {
+    write-host "La carpeta a copiar no existe" #Si no existe mando un mensaje de error
+
+}
