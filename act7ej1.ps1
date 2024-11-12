6ea5e01716ca215945bb14aec7d42c9c41b3711b
Rruben2 <rramosmombiela01.cf@iesesteveterradas.cat>
2024-11-12 16:08:49 +0100

    Add files via upload

---
+++ act7ej1.ps1
@@ -0,0 +1,29 @@
+﻿function calculadora
+
+{$numero1=$args[0]
+$numero2=$args[1]
+$operacion=$args[2]
+
+if ($operacion -eq "+"){
+    $resultado=[int]$numero1 + [int]$numero2
+    Write-Host "El resultado de su operacion es " $resultado
+}
+
+elseif ($operacion -eq "-"){
+    $resultado=[int]$numero1 - [int]$numero2
+    Write-Host "El resultado de su operacion es " $resultado
+}
+
+elseif ($operacion -eq "/"){
+    $resultado=[int]$numero1 / [int]$numero2
+    Write-Host "El resultado de su operacion es " $resultado
+}
+
+elseif ($operacion -eq "*"){
+    $resultado=[int]$numero1 * [int]$numero2
+    Write-Host "El resultado de su operacion es " $resultado
+}
+else {
+    Write-Host "Error de operacion el orden es este numero1 numero2 operacion( + - * /)"
+}
+}
---
+++ act7ej2.ps1
@@ -0,0 +1,17 @@
+﻿function buscar-archivo {
+    
+    $nombreArchivo = $args[0]
+
+    
+    $ruta = "C:\" 
+
+    $resultado = Get-ChildItem -Path $ruta -Recurse -Filter $nombreArchivo -ErrorAction SilentlyContinue
+
+    if ($resultado) {
+        Write-Host "El archivo existe en esta ruta:" $resultado.FullName
+    }
+    else {
+        Write-Host "No se encontró el archivo '$nombreArchivo'."
+    }
+}
+
