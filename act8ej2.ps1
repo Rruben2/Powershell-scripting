c3866f72c1520bb754efeec3a1842148cdb40d33
Rruben2 <rramosmombiela01.cf@iesesteveterradas.cat>
2024-11-19 16:42:54 +0100

    Add files via upload

---
+++ act8ej2.ps1
@@ -0,0 +1,14 @@
+﻿$puerto = 443
+$servidor = "google.com"
+
+Write-Host "Comprobando conexion"
+
+$conexion = Test-NetConnection -ComputerName $servidor -Port $puerto
+
+if ($conexion.TcpTestSucceeded) {
+Write-Host "La conexion esta en funcionamiento"
+}
+else
+{
+Write-Host "Error en la conexion TCP"
+}
---
+++ act8ej3.ps1
@@ -0,0 +1,19 @@
+﻿$tipop = Read-Host "Indica el tipo de puerto (TCP,UDP,OTROS)".ToUpper()
+
+if ($tipop -eq "TCP") {
+    Write-Host "Conexiones TCP:"
+    Get-NetTCPConnection | Where-Object State -eq "Listen" | Format-Table LocalAddress, LocalPort, State
+}
+elseif ($tipop -eq "UDP"){
+    Write-Host "Conexions UDP:"
+    Get-NetUDPEndpoint | Format-Table LocalAddress, LocalPort
+}
+elseif ($tipop -eq "OTROS"){
+    Write-Host "Conexiones TCP:"
+    Get-NetTCPConnection | Where-Object State -eq "Listen" | Format-Table LocalAddress, LocalPort, State
+    Write-Host "Conexions UDP:"
+    Get-NetUDPEndpoint | Format-Table LocalAddress, LocalPort
+}
+else {
+Write-Host "No hay ningun puerto con ese tipo"
+}
