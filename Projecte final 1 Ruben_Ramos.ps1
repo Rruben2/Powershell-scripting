433b9d9e2b57dc8f29086c9ca95f9d535a9e9b2f
Rruben2 <rramosmombiela01.cf@iesesteveterradas.cat>
2024-11-26 17:33:34 +0100

    Add files via upload

---
+++ Projecte final 1 Ruben_Ramos.ps1
@@ -0,0 +1,15 @@
+﻿# Nombre de la nueva máquina virtual
+$vm = "Maquina prueba win"
+
+# Crear la nueva máquina virtual con el nombre y tipo de sistema operativo
+vboxmanage createvm --name $vm --ostype "Windows10_64" --register
+
+# Crear un adaptador de red NAT para la VM (red predeterminada)
+vboxmanage modifyvm $vm --nic1 nat
+
+# Crear las 7 tarjetas de red adicionales
+for ($i = 2; $i -le 8; $i++) {
+    vboxmanage modifyvm $vm --nic$i intnet 
+}
+
+Write-Host "La máquina virtual '$vm' ha sido creada con 8 tarjetas de red."
---
+++ Projecte final 3 Ruben_Ramos.ps1
@@ -0,0 +1,28 @@
+﻿Write-Host "1.Iniciar VM"
+Write-Host "2.Apagar VM"
+Write-Host "3.Guardar estado de VM"
+$opcion= Read-Host "Eliga una opcion"
+
+if ($opcion -eq 1)
+{
+$vm = Read-Host "Eliga el nombre de la maquina a encender"
+ 
+vboxmanage startvm $vm
+
+}
+if ($opcion -eq 2)
+{
+$vm = Read-Host "Eliga el nombre de la maquina a apagar"
+ 
+vboxmanage stopvm $vm
+
+}
+
+if ($opcion -eq 3)
+{
+$vm = Read-Host "Eliga el nombre de la maquina a guardar estado"
+ 
+vboxmanage controlvm $vm savestate
+
+}
+
